b529a41cc23b2c520881656a4b701ae91ba62c10
yifange <irfan.1989@gmail.com>
2013-10-08 23:13:07 -0400

    kickstart

---
+++ mindless/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mindless::Application.config.secret_key_base = 'fa64fb0d3a24cbb3206e468ed4407f2461f007fa425c12a66c1a725ead3437403a90b99e458bafeb0d14d1b615fec0460cd319620311999b8475ba5e0b9c168c'
---
+++ mindless/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mindless::Application.config.session_store :cookie_store, key: '_mindless_session'
